485de96ba15bc429f64b05abc531793e58e072b2
Noah Keppers <nkeppers@gmail.com>
2018-09-16 18:40:14 -0400

    Add NgBootstrap and implement basic navbar

--- frontend/sentiment-dashboard/src/app/app.module.ts
+++ frontend/sentiment-dashboard/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
@@ -16,7 +17,8 @@ import { NavComponent } from './shared/nav/nav.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
