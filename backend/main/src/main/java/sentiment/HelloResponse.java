15689fcd1c1cb602dfae033869fe79e4bfbf912b
Noah Keppers <nkeppers@gmail.com>
2018-09-15 22:36:02 -0400

    Set up checkstyle java linter and fix existing files

--- backend/main/src/main/java/sentiment/HelloResponse.java
+++ backend/main/src/main/java/sentiment/HelloResponse.java
@@ -1,27 +1,25 @@
 package sentiment;
 
 /**
- * Response format for Hello, World lambda
+ * Response format for Hello, World lambda.
  */
 public class HelloResponse {
-    /**
-     * The message to retun from the API
-     */
-    private String message;
+  /**
+   * The message to retun from the API.
+   */
+  private String message;
 
-    public HelloResponse() {
+  public HelloResponse() { }
 
-    }
+  public HelloResponse(String message) {
+    this.message = message;
+  }
 
-    public HelloResponse(String message) {
-        this.message = message;
-    }
+  public String getMessage() {
+    return this.message;
+  }
 
-    public String getMessage() {
-        return this.message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
+  public void setMessage(String message) {
+    this.message = message;
+  }
 }
