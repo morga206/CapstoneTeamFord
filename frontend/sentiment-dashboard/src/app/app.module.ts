294d323694f14183637e174253c97ab671666fed
Noah Keppers <keppersn@msu.edu>
2018-09-21 15:35:23 -0400

    Added form to dashboard to set app name, version, date

--- frontend/sentiment-dashboard/src/app/app.module.ts
+++ frontend/sentiment-dashboard/src/app/app.module.ts
@@ -3,21 +3,25 @@ import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SettingsComponent } from './settings/settings.component';
 import { NavComponent } from './shared/nav/nav.component';
+import { DatepickerComponent } from './shared/datepicker/datepicker.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     SettingsComponent,
-    NavComponent
+    NavComponent,
+    DatepickerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     NgbModule
   ],
   providers: [],
