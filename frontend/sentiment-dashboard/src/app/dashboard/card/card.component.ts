d44642bda0c2650f7ec31193ca4eaacdf905f78c
Noah Keppers <nkeppers@gmail.com>
2018-09-23 14:10:50 -0400

    Add Top Keywords layout and fix responsiveness

--- frontend/sentiment-dashboard/src/app/dashboard/card/card.component.ts
+++ frontend/sentiment-dashboard/src/app/dashboard/card/card.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit, Input, HostBinding, Host } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
-  @HostBinding('class') classes = 'card card-body mr-2';
+  @HostBinding('class') classes = 'card card-body mr-4 mb-md-4';
   @Input() title: String = '';
 
   constructor() { }
