c974ac5a0c86d6589fb4a3a9b457d00902a43889
Noah Keppers <nkeppers@gmail.com>
2018-09-23 14:38:39 -0400

    A11y fixes

--- frontend/sentiment-dashboard/src/app/shared/datepicker/datepicker.component.ts
+++ frontend/sentiment-dashboard/src/app/shared/datepicker/datepicker.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-datepicker',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./datepicker.component.scss']
 })
 export class DatepickerComponent implements OnInit {
+  @Input() controlId = '';
   model;
 
   constructor() { }
