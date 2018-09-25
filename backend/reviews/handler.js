c60228341ee19c7bddd56e7a5448425986400c42
Noah Keppers <nkeppers@gmail.com>
2018-09-25 18:57:35 -0400

    ESLint setup + first pass on code

--- backend/reviews/handler.js
+++ backend/reviews/handler.js
@@ -1,14 +1,48 @@
 'use strict';
 
-module.exports.hello = async (event, context) => {
+const appStoreScraper = require('app-store-scraper');
+const gPlayScraper = require('google-play-scraper');
+
+const START_PAGE = 0;
+const MAX_PAGE = 9;
+
+const APP_STORE_IDS = [1095418609]; // TODO get this from config DB
+const GPLAY_IDS = ['com.ford.fordpass']; // TODO get this from config DB
+
+module.exports.handler = async () => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
+      message: scrapeApps()
     }),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+function scrapeApps() {
+  let reviews = [];
+
+  for (let appId in APP_STORE_IDS) {
+    reviews.concat(scrape(appId, appStoreScraper));
+  }
+
+  for (let appId in GPLAY_IDS) {
+    reviews.concat(scrape(appId, gPlayScraper));
+  }
+
+  return reviews;
+}
+
+function scrape(appId, scraper) {
+  let reviews = [];
+
+  for (let i = START_PAGE; i <= MAX_PAGE; i++) {
+    scraper.reviews({
+      appId: appId,
+      page: i
+    }).then (pageReviews => {
+      reviews.concat(pageReviews);
+    });
+  }
+
+  return reviews;
+}
