d71c8842cad92b210a1e9232f9183c695dee964c
Noah Keppers <nkeppers@gmail.com>
2018-09-28 20:21:05 -0400

    Fix linter errors

--- frontend/sentiment-dashboard/src/app/dashboard/dashboard.component.ts
+++ frontend/sentiment-dashboard/src/app/dashboard/dashboard.component.ts
@@ -10,7 +10,7 @@ export class DashboardComponent implements OnInit {
   public pieChartLabels: string[] = ['Very Positive', 'Positive', 'Neutral', 'Negative', 'Very Negative'];
   public pieChartData: number[] = [20, 40, 40, 5, 5];
   public pieChartType = 'pie';
-  public pieChartColors:Array<any> = [
+  public pieChartColors: Array<any> = [
     { backgroundColor: ['rgba(0,229,0,1)', 'rgba(83,204,65,1)', 'rgba(200, 200, 200, 1)', 'rgba(158,0,0,1)', 'rgba(204,65,65,1)'] }
   ];
 
@@ -18,12 +18,13 @@ export class DashboardComponent implements OnInit {
     { data: [100, 75, 40, 50, 72, 67, 80], label: 'Positive' },
     { data: [0, 25, 20, 50, 28, 42, 20], label: 'Negative' }
   ];
-  public lineChartLabels: Array<any> = ['September 1', 'September 2', 'September 3', 'September 4', 'September 5', 'September 6', 'September 7'];
+  public lineChartLabels: Array<any> =
+    ['September 1', 'September 2', 'September 3', 'September 4', 'September 5', 'September 6', 'September 7'];
   public lineChartType = 'line';
   public lineChartOptions: any = {
     responsive: true
   };
-  public lineChartColors:Array<any> = [
+  public lineChartColors: Array<any> = [
     { // green
       backgroundColor: 'rgba(0,0,0,0)',
       borderColor: 'rgba(83,204,65,1)',
