1fc13930b18663596fa689381d0db650b5663e89
Jordan Schroeder <jsch3475@gmail.com>
2018-10-10 20:39:06 -0400

    Fix errors

--- frontend/sentiment-dashboard/src/app/shared/nav/nav.component.ts
+++ frontend/sentiment-dashboard/src/app/shared/nav/nav.component.ts
@@ -11,7 +11,7 @@ export class NavComponent implements OnInit {
   public isCollapsed = true;
 
   constructor(
-    private auth: AuthService
+    public auth: AuthService
   ) { }
 
   ngOnInit() {
