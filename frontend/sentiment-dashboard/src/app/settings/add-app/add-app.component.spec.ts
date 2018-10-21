2b1b4be3df4d6f5aedaa089d3421e282942b1ca9
Noah Keppers <nkeppers@gmail.com>
2018-10-20 23:26:26 -0400

    Add app modal test

--- frontend/sentiment-dashboard/src/app/settings/add-app/add-app.component.spec.ts
+++ frontend/sentiment-dashboard/src/app/settings/add-app/add-app.component.spec.ts
@@ -3,6 +3,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AddAppComponent } from './add-app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgbModule, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { By } from '@angular/platform-browser';
+import { App } from 'src/app/rest/domain';
 
 describe('AddAppComponent', () => {
   let component: AddAppComponent;
@@ -25,5 +27,49 @@ describe('AddAppComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be value when all fields are filled', () => {
+    component.appName.setValue('Test Name');
+    component.appStore.setValue('Google Play');
+    component.appId.setValue('com.ford.test');
+    component.addAppForm.updateValueAndValidity();
+
+    expect(component.addAppForm.valid).toBeTruthy();
+  });
+
+  it('should be invalid if any field is not filled out', () => {
+    expect(component.addAppForm.valid).toBeFalsy();
+
+    component.appName.setValue('Test Name');
+    component.addAppForm.updateValueAndValidity();
+    expect(component.addAppForm.valid).toBeFalsy();
+
+    component.appStore.setValue('Google Play');
+    component.addAppForm.updateValueAndValidity();
+    expect(component.addAppForm.valid).toBeFalsy();
+
+    component.appName.setValue('');
+    component.appId.setValue('com.ford.test');
+    component.addAppForm.updateValueAndValidity();
+    expect(component.addAppForm.valid).toBeFalsy();
+
+    component.appName.setValue('Test Name');
+    component.addAppForm.updateValueAndValidity();
+    expect(component.addAppForm.valid).toBeTruthy();
+  });
+
+  it('should not submit if the appId format is invalid', () => {
+    component.appName.setValue('Test Name');
+    component.appStore.setValue('Google Play');
+    component.appId.setValue('com.ford.test&');
+    component.addAppForm.updateValueAndValidity();
+
+    expect(component.addAppForm.valid).toBeFalsy();
+
+    component.appId.setValue('com.ford.test123');
+    component.addAppForm.updateValueAndValidity();
+
+    expect(component.addAppForm.valid).toBeTruthy();
   });
 });
