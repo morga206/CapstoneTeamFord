6062b0b85cf26bee14a94bac9fbb2e5788f1ebb0
Noah Keppers <nkeppers@gmail.com>
2018-11-11 11:41:43 -0500

    Fix test errors

--- backend/slackbot/tests/test.js
+++ backend/slackbot/tests/test.js
@@ -1,7 +1,9 @@
 'use strict';
 var assert = require('assert');
 
-var handler = require('../worker');
+var worker = require('../worker');
 
 describe('worker', function() {
+  console.log(worker);
+  console.log(assert);
 });
